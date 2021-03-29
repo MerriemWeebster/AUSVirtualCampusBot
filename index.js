b5c184c59e492503b859d486510ac71623bd692f
MerriemWeebster <daniyalkhan2000@users.noreply.github.com>
2021-03-29 23:03:33 +0500

    Fix

--- index.js
+++ index.js
@@ -456,7 +456,13 @@ client.on('message', msg => {
             if (msg.content.toLowerCase() === 'aus/startevent' && msg.channel.id == "822443774507614218") 
             {
                 var category = msg.guild.channels.resolve(eventChannel).parent;
-                category.overwritePermissions([{id: "822441807300001793", allow: ['SEND_MESSAGES', 'CONNECT', 'VIEW_CHANNEL']}, {id: "821983751147356171", deny: ['VIEW_CHANNEL']}])
+                var channels = category.children.array();
+
+                for(var i = 0; i < channels.length; i++)
+                {
+                    channels[i].overwritePermissions([{id: "822441807300001793", allow: ['SEND_MESSAGES', 'CONNECT', 'VIEW_CHANNEL']}, {id: "821983751147356171", deny: ['VIEW_CHANNEL']}])
+                }
+                
                 msg.reply("Event started")
             }
 
@@ -469,7 +475,13 @@ client.on('message', msg => {
                 eventScores = [];
 
                 var category = msg.guild.channels.resolve(eventChannel).parent;
-                category.overwritePermissions([{id: "822441807300001793", deny: ['SEND_MESSAGES', 'CONNECT'], allow: ['VIEW_CHANNEL']}, {id: "821983751147356171", deny: ['VIEW_CHANNEL']}])
+                var channels = category.children.array();
+
+                for(var i = 0; i < channels.length; i++)
+                {
+                    channels[i].overwritePermissions([{id: "822441807300001793", deny: ['SEND_MESSAGES', 'CONNECT'], allow: ['VIEW_CHANNEL']}, {id: "821983751147356171", deny: ['VIEW_CHANNEL']}])
+                }
+
 
                 msg.reply("Event reset")
             }
